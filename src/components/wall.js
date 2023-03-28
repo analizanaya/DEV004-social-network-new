a4fbcc9c871240526ed71e6c491c046e036884d2
Lizet Peche <lizet.peche@gmail.com>
2023-03-28 11:17:03 -1100

    pruebas unitarias

--- src/components/wall.js
+++ src/components/wall.js
@@ -1,20 +1,20 @@
 export const wall = (onNavigate) => {
-  const div = document.createElement("div");
-  const title = document.createElement("h2");
-  const button = document.createElement("button");
-  const buttonBack = document.createElement("button");
-  const inputEmail = document.createElement("input");
-  const inputPass = document.createElement("input");
-  button.textContent = "Login";
-  buttonBack.textContent = "Regresa";
-  title.textContent = "Hola vista 3";
+  const div = document.createElement('div');
+  const title = document.createElement('h2');
+  const button = document.createElement('button');
+  const buttonBack = document.createElement('button');
+  const inputEmail = document.createElement('input');
+  const inputPass = document.createElement('input');
+  button.textContent = 'Login';
+  buttonBack.textContent = 'Regresa';
+  title.textContent = 'Hola vista 3';
 
-  buttonBack.addEventListener("click", () => {
-    onNavigate("/");
+  buttonBack.addEventListener('click', () => {
+    onNavigate('/');
   });
 
-  button.addEventListener("click", () => {
-    onNavigate("/");
+  button.addEventListener('click', () => {
+    onNavigate('/');
   });
 
   div.append(title, inputEmail, inputPass, button, buttonBack);
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,16 @@
 // importamos la funcion que vamos a testear
 import { Welcome } from "../src/components/Welcome.js";
-import { logincreateUserWithEmailAndPassword } from "../src/Firebase/firebase.js";
+import * as authentication from "../src/Firebase/firebase.js";
+import * as router from "../src/main.js";
 //mock
 describe("Pruebas de login", () => {
   beforeEach(() => {
-    authentication.signInWithEmailAndPassword = jest.fn();
-    authentication.signInWithPopup = jest.fn();
+    authentication.loginWithEmailAndPassword = jest.fn();
     router.onNavigate = jest.fn(() => console.log("mock de onNavigate usado"));
   });
   it("Autenticación con correo electrónico y contraseña correcta, debería redireccionar a /wall", () => {
     //preparamos el mock
-    authentication.signInWithEmailAndPassword.mockResolvedValueOnce({
+    authentication.loginWithEmailAndPassword.mockResolvedValueOnce({
       user: { email: "p@gmail.com" },
     });
 
@@ -22,17 +22,11 @@ describe("Pruebas de login", () => {
     divLogin.querySelector("#password").value = "123456";
 
     //Paso 3: Enviamos el formulario dando clic en el botón `Login`.
-    div.querySelector("#section").dispatchEvent(new Event("click"));
+    div.querySelector(".buttonGetinto").dispatchEvent(new Event("click"));
 
     //Paso 4: Verificamos visualmente que la aplicación redija a `/home`.
     return Promise.resolve().then(() =>
       expect(router.onNavigate).toHaveBeenCalledWith("/wall")
     );
   });
-});
-
-describe("myFunction", () => {
-  it("debería ser una función", () => {
-    expect(window.location.pathname).toEqual("/wall");
-  });
-});
+}); 
