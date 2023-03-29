4af7cbc1c0c0b5cf3f5df37080fcb443efea53cc
analizanaya <lizinayari@gmail.com>
2023-03-29 12:35:00 -0500

    FIX: corrección parcial test

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,25 +1,31 @@
 // importamos la funcion que vamos a testear
   import * as router from "../src/router";
-
+import { signInWithEmailAndPassword } from "firebase/auth";
   import { Welcome } from '../src/components/Welcome.js';
 
   describe('Pruebas de login', () => {
 
     beforeEach(() => {
       //authentication.signInWithGoogle = jest.fn();
-      signInWithEmailAndPassword = jest.fn();
+      
+      //getAuth().signInWithEmailAndPassword = jest.fn();
+      jest.mock("../src/Firebase/firebase", ()=>({
+        auth: {
+          signInWithEmailAndPassword:jest.fn()
+        }
+      }))
       router.onNavigate = jest.fn(() => console.log('mock de onNavigate usado'));
     });  
 
     it('Autenticación con correo electrónico y contraseña correcta, debería redireccionar a /wall', () => {
       //preparamos el mock
-      signInWithEmailAndPassword.mockResolvedValueOnce({ user: { email: 'p@gmail.com' } });
+      signInWithEmailAndPassword.mockResolvedValue({ user: { email: 'd@gmail.com' } });
 
       //Paso 1: Visualizar el formulario de login.
       const divLogin = Welcome();
 
       //Paso 2: Completamos el formulario con un correo electrónico y contraseña correctos.
-      divLogin.querySelector('#username').value = 'p@gmail.com';
+      divLogin.querySelector('#username').value = 'd@gmail.com';
       divLogin.querySelector('#password').value = '123456';  
       
       //Paso 3: Enviamos el formulario dando clic en el botón `Login`.
