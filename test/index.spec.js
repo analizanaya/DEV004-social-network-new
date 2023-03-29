4314170205a785d5405d98c9a103587a35239341
Lizet Peche <lizet.peche@gmail.com>
2023-03-28 13:04:08 -1100

    test trabajado

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,32 +1,38 @@
 // importamos la funcion que vamos a testear
-import { Welcome } from "../src/components/Welcome.js";
-import * as authentication from "../src/Firebase/firebase.js";
-import * as router from "../src/main.js";
-//mock
-describe("Pruebas de login", () => {
+import { Welcome } from '../src/components/Welcome.js';
+import * as authentication from '../src/Firebase/firebase.js';
+import * as router from '../src/main.js';
+
+// mock
+
+describe('Pruebas de login', () => {
   beforeEach(() => {
+    document.body.innerHTML = '<main id="root"></main>'
     authentication.loginWithEmailAndPassword = jest.fn();
-    router.onNavigate = jest.fn(() => console.log("mock de onNavigate usado"));
+    router.onNavigate = jest.fn(() => console.log('mock de onNavigate usado'));
+
   });
-  it("Autenticación con correo electrónico y contraseña correcta, debería redireccionar a /wall", () => {
-    //preparamos el mock
+  it('Autenticación con correo electrónico y contraseña correcta, debería redireccionar a /wall', () => {
+    const root = document.createElement('main');
+    root.id = 'root';
+    document.body.append(root);
+    // preparamos el mock
     authentication.loginWithEmailAndPassword.mockResolvedValueOnce({
-      user: { email: "p@gmail.com" },
+      user: { email: 'p@gmail.com' },
     });
+    console.log(document.body.innerHTML = "<main id='root'></main>")
+    // Paso 1: Visualizar el formulario de login.
+    const divLogin = Welcome(router.onNavigate);
 
-    //Paso 1: Visualizar el formulario de login.
-    const divLogin = Welcome();
+    // Paso 2: Completamos el formulario con un correo electrónico y contraseña correctos.
+    divLogin.querySelector('#username').value = 'p@gmail.com';
+    divLogin.querySelector('#password').value = '123456';
 
-    //Paso 2: Completamos el formulario con un correo electrónico y contraseña correctos.
-    divLogin.querySelector("#username").value = "p@gmail.com";
-    divLogin.querySelector("#password").value = "123456";
+    // Paso 3: Enviamos el formulario dando clic en el botón `Login`.
 
-    //Paso 3: Enviamos el formulario dando clic en el botón `Login`.
-    div.querySelector(".buttonGetinto").dispatchEvent(new Event("click"));
+    divLogin.querySelector('.buttonGetinto').dispatchEvent(new Event('click'));
 
-    //Paso 4: Verificamos visualmente que la aplicación redija a `/home`.
-    return Promise.resolve().then(() =>
-      expect(router.onNavigate).toHaveBeenCalledWith("/wall")
-    );
+    // Paso 4: Verificamos visualmente que la aplicación redija a `/home`.
+   return Promise.resolve().then(() => expect(router.onNavigate).toHaveBeenCalledWith('/wall'));
   });
 }); 
