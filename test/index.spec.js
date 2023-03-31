78ddc41a358c2fdf434b518bbb664bf9e8f928b8
Lizet Peche <lizet.peche@gmail.com>
2023-03-30 16:19:56 -1100

    test sobre 90%

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,27 +1,39 @@
 import * as router from "../src/router";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { Welcome } from '../src/components/Welcome.js';
+import {loginWithEmailAndPassword} from '../src/Firebase/authentication.js';
+
+//const mockSignInWithEmailAndPassword =jest.fn();
+jest.mock('firebase/auth', ()=>({
+    getAuth: jest.fn(),
+    signInWithEmailAndPassword:jest.fn(),
+    GoogleAuthProvider:jest.fn(),
+}))
+
+jest.mock('../src/Firebase/authentication.js', ()=>({
+  loginWithEmailAndPassword:jest.fn(() => Promise.resolve())
+}))
 
   describe('Pruebas de login', () => {
 
-    it('Autenticación con correo electrónico y contraseña correcta, debería redireccionar a /wall', async () => {
+    it('Autenticación con correo electrónico y contraseña correcta, debería redireccionar a /wall', (done) => {
       //mocks
-      const mockSignInWithEmailAndPassword =jest.fn();
-      jest.mock('firebase/auth', ()=>({
-          getAut: jest.fn(),
-          signInWithEmailAndPassword:mockSignInWithEmailAndPassword
-       
-      }))
-      router.onNavigate = jest.fn(() => console.log('mock de onNavigate usado'));
+
+      //loginWithEmailAndPassword = jest.fn(() => Promise.resolve())
+      router.onNavigate = jest.fn((route) => console.log(route,'mock de onNavigate usado'));
       //preparamos el mock
-      mockSignInWithEmailAndPassword.mockResolvedValueOnce({ user: { email: 'd@gmail.com' } });
+      //signInWithEmailAndPassword.mockResolvedValueOnce({ user: { email: 'd@gmail.com' } })
 
       const divLogin = Welcome();
 
       divLogin.querySelector('#username').value = 'd@gmail.com';
       divLogin.querySelector('#password').value = '123456';  
-      divLogin.querySelector('.buttonGetinto').dispatchEvent(new Event('click'));
+      divLogin.querySelector('.buttonGetinto').click();
 
-      await Promise.resolve().then(() => expect(router.onNavigate).toHaveBeenCalledWith('/wall'));
+      setTimeout(() => {
+        expect(router.onNavigate).toHaveBeenCalledWith('/wall');
+        done();
+      }, 0);
+      
     });
   });
