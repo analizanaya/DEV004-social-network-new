c08b0ad92ba2ff6eec338d015d5db5a8097d48ac
Yesenia <lizet.peche@gmail.com>
2023-04-17 19:05:23 -0500

    se agrego scroll

--- src/components/wall.js
+++ src/components/wall.js
@@ -3,14 +3,11 @@ import { post, getPost } from "../Firebase/authentication";
 import { onNavigate } from "../router.js";
 import { collection, query, onSnapshot } from "firebase/firestore";
 import { userPosts } from "../store/userData.js";
-
 let buttonSend = document.createElement("button");
 let inputShowModal = document.createElement("textarea");
-
 buttonSend.addEventListener("click", () => {
   const task = document.createElement("p");
   task.textContent = inputShowModal.value;
-
   const taskContainer = document.querySelector("#taskContainer"); // Obtener el elemento que contenerá las tareas
   if (taskContainer) {
     // Verificar si el elemento existe
@@ -19,10 +16,8 @@ buttonSend.addEventListener("click", () => {
   } else {
     console.error("No se encontró el elemento que contiene las tareas");
   }
-
   inputShowModal.value = "";
 });
-
 export const wall = () => {
   const div = document.createElement("div");
   const dialog = document.createElement("dialog");
@@ -36,20 +31,17 @@ export const wall = () => {
   const imgUser = document.createElement("img");
   const logo2 = document.createElement("img");
   const fondo = document.createElement("img");
-  //const likeEmptyIcon = document.createElement("img", "input");
+  const likeEmptyIcon = document.createElement("img", "input");
   const likeFullIcon = document.createElement("img", "input");
   const commentIcon = document.createElement("img", "input");
   const buttonSingOff = document.createElement("button");
   const inputComment = document.createElement("input");
-
   let buttonsShowModal = document.createElement("img", "button");
   let inputPost = document.createElement("input");
   let buttonSend = document.createElement("button");
   let inputShowModal = document.createElement("textarea");
-
   inputShowModal.placeholder = "¿ Qué estás pensando ... ?";
   inputPost.placeholder = "¿ Qué estás pensando ... ?";
-
   inputPost.type = "texto";
   inputComment.type = "texto";
   adjustmentButtons.type = "button";
@@ -57,7 +49,6 @@ export const wall = () => {
   buttonsShowModal.type = "button";
   buttonxIcon.type = "button";
   buttonxIcon2.type = "button";
-
   fondo.id = "fondo";
   div.id = "section";
   logo2.id = "logo2";
@@ -67,12 +58,10 @@ export const wall = () => {
   inputComment.id = "comment";
   imgUser.id = "imgUser";
   taskContainer.id = "taskContainer";
-
   buttonSend.textContent = "SEND";
   buttonEdit.textContent = "Edit";
   buttonDelete.textContent = "Delete";
   buttonSingOff.textContent = "Cerrar Sesión";
-
   buttonSend.className = "send";
   buttonxIcon.className = "buttonX";
   buttonxIcon2.className = "buttonX2";
@@ -87,63 +76,47 @@ export const wall = () => {
   adjustmentButtons.className = "adjustmentButtonsIcon";
   buttonsShowModal.className = "ButtonsShowModal";
   dialogAjustes.className = "dialogAjustes";
-
   //agregado
   imgUser.src = "./imagenes/user.png";
   imgUser.alt = "imgUser";
-
   adjustmentButtons.src = "./imagenes/adjustmentButtonsIcon.png";
   adjustmentButtons.alt = "adjustmentButtons";
-
   logo2.src = "./imagenes/logo.png";
   logo2.alt = "Logo";
-
   fondo.src = "./imagenes/fondo-cel.png";
   fondo.alt = "Fondo";
-
- /*  likeEmptyIcon.src = "./imagenes/likeVacio.png";
+  likeEmptyIcon.src = "./imagenes/likeVacio.png";
   likeEmptyIcon.alt = "Like1";
- */
   likeFullIcon.src = "./imagenes/likeLleno.png";
   likeFullIcon.alt = "Like2";
   likeFullIcon.style.display = "none";
-
   commentIcon.src = "./imagenes/comentario.png";
   commentIcon.alt = "comentario";
-
   buttonxIcon.src = "./imagenes/x.png";
   buttonxIcon.alt = "equis";
-
   buttonxIcon2.src = "./imagenes/x.png";
   buttonxIcon2.alt = "equis";
-
   adjustmentButtons.addEventListener("click", function () {
     dialogAjustes.showModal();
   });
   inputPost.addEventListener("click", function () {
     dialog.showModal();
   });
-
   buttonSend.addEventListener("click", () => {
     post(inputShowModal.value).then((response) => {
       return response;
     });
-
     dialog.close();
   });
-
   buttonxIcon.addEventListener("click", function () {
     dialog.close();
   });
-
   buttonxIcon2.addEventListener("click", function () {
     dialogAjustes.close();
   });
-
   buttonSingOff.addEventListener("click", () => {
     onNavigate("/");
   });
-
   dialog.appendChild(inputShowModal);
   dialog.appendChild(buttonSend);
   dialog.appendChild(buttonxIcon);
@@ -151,7 +124,6 @@ export const wall = () => {
   dialogAjustes.appendChild(buttonEdit);
   dialogAjustes.appendChild(buttonDelete);
   dialogAjustes.appendChild(buttonxIcon2);
-
   div.append(
     dialog,
     dialogAjustes,
@@ -164,7 +136,6 @@ export const wall = () => {
     inputComment,
     buttonSingOff
   );
-
   getPost((querySnapshot) => {
     taskContainer.innerHTML = "";
     const posts = [];
@@ -174,24 +145,17 @@ export const wall = () => {
     });
     const prueba = posts.forEach((publicacion) => {
       const padre = document.createElement("div");
-      Contador = Contador +1;
       const input = document.createElement("textarea");
-      const likeEmptyIcon = document.createElement("img", "input");
-      likeEmptyIcon.src = "./imagenes/likeVacio.png";
-      likeEmptyIcon.alt = "Like1";
-      likeEmptyIcon.className = "like1";
-      /* const likeEmptyIconClone = likeEmptyIcon.cloneNode(true);
+      const likeEmptyIconClone = likeEmptyIcon.cloneNode(true);
       const likeFullIconClone = likeFullIcon.cloneNode(true);
-      const commentIconClone = commentIcon.cloneNode(true); */
-
+      const commentIconClone = commentIcon.cloneNode(true);
       input.id = "comments";
-      padre.id = "padre-" + Contador;
-      
+      input.rows = 1; // Valor inicial
+      padre.id = "padre";
       input.value = publicacion;
+    
       console.log(publicacion);
-
       let liked = false;
-
       likeEmptyIconClone.addEventListener("click", () => {
         if (!liked) {
           likeFullIconClone.src = "./imagenes/likeLleno.png";
@@ -202,7 +166,6 @@ export const wall = () => {
         } else {
         }
       });
-
       likeFullIconClone.addEventListener("click", () => {
         if (liked) {
           likeEmptyIconClone.src = "./imagenes/likeVacio.png";
@@ -213,15 +176,21 @@ export const wall = () => {
         } else {
         }
       });
-
       padre.appendChild(input);
-      padre.appendChild(likeEmptyIcon);
-      /* padre.appendChild(likeFullIconClone);
-      padre.appendChild(commentIconClone); */
-
+      padre.appendChild(likeEmptyIconClone);
+      padre.appendChild(likeFullIconClone);
+      padre.appendChild(commentIconClone);
       taskContainer.appendChild(padre);
+      
+      input.addEventListener("input", () => { // añadir listener revisarlo al final
+        input.style.height = "auto";
+        /* input.style.height = `${input.scrollHeight}px`; */
+      });
     });
 
   });
+
+ 
+
   return div;
 };
