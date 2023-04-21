512f0f70c77bce797bb89cdfb3d990d32a52fd25
analizanaya <lizinayari@gmail.com>
2023-04-21 12:05:44 -0500

    problema id firestore

--- src/Firebase/authentication.js
+++ src/Firebase/authentication.js
@@ -2,7 +2,7 @@ import {
   getAuth, createUserWithEmailAndPassword, GoogleAuthProvider, signInWithPopup,
   signInWithEmailAndPassword, onAuthStateChanged
 } from 'firebase/auth';
-import { collection, addDoc, query, onSnapshot, doc, deleteDoc } from "firebase/firestore";
+import { collection, addDoc, query, onSnapshot, doc, deleteDoc, updateDoc } from "firebase/firestore";
 import { db } from './firebase';
 import { userData } from '../store/userData.js';
 
@@ -77,7 +77,11 @@ export function getPost(callBack){
 }
 export const deletePosta = id => deleteDoc(doc ( db,'Publicaciones',id));
 
+export const postRef = doc(db, 'Publicaciones', id);
 
+await updateDoc(postRef, {
+  contenido: inputShowModal,
+});
 
 
  
