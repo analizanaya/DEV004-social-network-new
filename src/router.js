3ad7b8e5025047d6ff78e99f647b30dc245d9344
analizanaya <lizinayari@gmail.com>
2023-06-05 08:42:02 -0500

    solucionando problemas EsLint

--- src/router.js
+++ src/router.js
@@ -1,8 +1,6 @@
 const LOCAL_ROUTES = {};
 
 export const onNavigate = (pathname, updateHistory = true) => {
-
-  // If the path is not found, redirect to the home page
   const path = typeof LOCAL_ROUTES[pathname] !== 'function' ? pathname : '/';
 
   // Update the history
@@ -14,20 +12,17 @@ export const onNavigate = (pathname, updateHistory = true) => {
   const rootSection = document.getElementById('root');
   rootSection.innerHTML = '';
   rootSection.append(LOCAL_ROUTES[pathname]());
-
 };
 
 // Initialize the router with the routes
 export const initRouter = (routes) => {
-
   // Add routes to LOCAL_ROUTES
   Object.keys(routes).reduce((currentRoutes, pathname) => {
     currentRoutes[pathname] = routes[pathname];
     return currentRoutes;
   }, LOCAL_ROUTES);
 
-  // Add event listener to handle back/forward button
-  window.addEventListener('popstate', (e) => {
+  window.addEventListener('popstate', () => {
     onNavigate(window.location.pathname, false);
   });
 
@@ -35,4 +30,4 @@ export const initRouter = (routes) => {
   window.addEventListener('load', () => {
     onNavigate(window.location.pathname, false);
   });
-}
+};
